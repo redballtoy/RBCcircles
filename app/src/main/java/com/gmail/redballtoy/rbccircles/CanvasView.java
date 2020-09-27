1668168d01adad1c9f6d10880183f06eebf624b4
Dmitry Sorokin <redballtoy@gmail.com>
2020-09-27 21:29:50 +0300

    InitCommit

---
+++ app/src/main/java/com/gmail/redballtoy/rbccircles/CanvasView.java
@@ -0,0 +1,19 @@
+package com.gmail.redballtoy.rbccircles;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CanvasView extends View {
+    public CanvasView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
